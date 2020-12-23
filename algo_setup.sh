3af79b52f73fa219fa716abb49df1baf074b4ec5
moshe.nas <moshen733@gmail.com>
2020-12-24 01:39:00 +0200

    fixed algo_setup.sh

--- algo_setup.sh
+++ algo_setup.sh
@@ -2,15 +2,19 @@
 
 # installing torch
 wget https://nvidia.box.com/shared/static/wa34qwrwtk9njtyarwt5nvo6imenfy26.whl -O torch-1.7.0-cp36-cp36m-linux_aarch64.whl
-apt-get install libopenblas-base libopenmpi-dev
+apt-get install -y libopenblas-base libopenmpi-dev
 pip3 install Cython
 pip3 install torch-1.7.0-cp36-cp36m-linux_aarch64.whl
 
+# installing git
+apt update
+apt install -y git
+
 # installing torchvision
-apt-get install libjpeg-dev zlib1g-dev libpython3-dev libavcodec-dev libavformat-dev libswscale-dev
+apt-get install -y libjpeg-dev zlib1g-dev libpython3-dev libavcodec-dev libavformat-dev libswscale-dev
 git clone --branch v0.8.1 https://github.com/pytorch/vision torchvision
 cd torchvision
-export BUILD_VERSION=0.8.1
+export BUILD_VERSION=0.8.0
 python3 setup.py install
 cd ../
 
@@ -20,7 +24,7 @@ cd torch2trt
 python3 setup.py install --plugins
 
 pip3 install tqdm pycocotools
-apt-get install python3-matplotlib
+apt-get install -y python3-matplotlib
 cd ../
 
 # installing trt_pose
